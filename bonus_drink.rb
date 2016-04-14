58013388a3c69a79c721215e9abfd4fca2875aab
masaru shinbori <masaru.shinbori@monetize.co.jp>
2016-04-14 19:17:29 +0900

    とりあえずスペック通した
    もう算数が絶望的に出来なくて泣けた

--- bonus_drink.rb
+++ bonus_drink.rb
@@ -1,5 +1,24 @@
 class BonusDrink
   def self.total_count_for(amount)
-    0
+    bonus = amount
+    while bonus >= 3
+      amount += (bonus / 3)
+      bonus = (bonus % 3) + (bonus / 3)
+    end
+    amount
   end
+
+  #begin
+  #  print "0,",   BonusDrink.total_count_for(0),"\n"
+  #  print "1,",   BonusDrink.total_count_for(1),"\n"
+  #  print "3,",   BonusDrink.total_count_for(3),"\n"
+  #  print "4,",   BonusDrink.total_count_for(4),"\n"
+  #  print "5,",   BonusDrink.total_count_for(5),"\n"
+  #  print "6,",   BonusDrink.total_count_for(6),"\n"
+  #  print "7,",   BonusDrink.total_count_for(7),"\n"
+  #  print "8,",   BonusDrink.total_count_for(8),"\n"
+  #  print "11,",  BonusDrink.total_count_for(11),"\n"
+  #  print "100,", BonusDrink.total_count_for(100),"\n"
+  #end
+
 end
--- spec/bonus_drink_spec.rb
+++ spec/bonus_drink_spec.rb
@@ -1,5 +1,27 @@
 require File.expand_path(File.dirname(__FILE__) + '/../bonus_drink')
 
 describe BonusDrink do
-  specify { expect(BonusDrink.total_count_for(0)).to eq 0 }
+  context "0本" do
+    specify { expect(BonusDrink.total_count_for(0)).to eq 0 }
+  end
+  context "1本" do
+    specify { expect(BonusDrink.total_count_for(1)).to eq 1 }
+  end
+  context "3本" do
+    specify { expect(BonusDrink.total_count_for(3)).to eq 4 }
+  end
+  # 11本の時は3本毎で交換後にもう一度3本の固まりがあるときに再交換できるんで14本にはならない
+  context "11本" do
+    specify { expect(BonusDrink.total_count_for(11)).to eq 16 }
+  end
+
+  context "5本" do
+    specify { expect(BonusDrink.total_count_for(5)).to eq 7 }
+  end
+  context "6本" do
+    specify { expect(BonusDrink.total_count_for(6)).to eq 8 }
+  end
+  context "8本" do
+    specify { expect(BonusDrink.total_count_for(8)).to eq 11 }
+  end
 end
