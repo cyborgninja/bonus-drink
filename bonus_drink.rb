1c564498974ddc58d4ee512c8235109037b0f9ea
Shintaro SOYAMA <soyamagic.super@gmaill.com>
2016-10-25 15:52:26 +0900

    Create calc.rb

--- bonus_drink.rb
+++ bonus_drink.rb
@@ -5,20 +5,6 @@ class BonusDrink
       amount += (bonus / 3)
       bonus = (bonus % 3) + (bonus / 3)
     end
-    amount
+    print "[result]", amount, "\n"
   end
-
-  #begin
-  #  print "0,",   BonusDrink.total_count_for(0),"\n"
-  #  print "1,",   BonusDrink.total_count_for(1),"\n"
-  #  print "3,",   BonusDrink.total_count_for(3),"\n"
-  #  print "4,",   BonusDrink.total_count_for(4),"\n"
-  #  print "5,",   BonusDrink.total_count_for(5),"\n"
-  #  print "6,",   BonusDrink.total_count_for(6),"\n"
-  #  print "7,",   BonusDrink.total_count_for(7),"\n"
-  #  print "8,",   BonusDrink.total_count_for(8),"\n"
-  #  print "11,",  BonusDrink.total_count_for(11),"\n"
-  #  print "100,", BonusDrink.total_count_for(100),"\n"
-  #end
-
 end
