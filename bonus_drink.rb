cba7f186ee9573629ae443174d35186ad1d6d085
masaru shinbori <masaru.shinbori@favy.co.jp>
2018-01-24 17:47:29 +0900

    feat(logic): ロジックちょっと変更
    ロジック変えたぜ

--- bonus_drink.rb
+++ bonus_drink.rb
@@ -1,24 +1,26 @@
 class BonusDrink
   def self.total_count_for(amount)
-    bonus = amount
-    while bonus >= 3
-      amount += (bonus / 3)
-      bonus = (bonus % 3) + (bonus / 3)
-    end
-    amount
+    # bonus = amount
+    # while bonus >= 3
+    #   amount += (bonus / 3)
+    #   bonus = (bonus % 3) + (bonus / 3)
+    # end
+    # amount
+    amount + (amount-1).abs / 2
   end
 
-  #begin
-  #  print "0,",   BonusDrink.total_count_for(0),"\n"
-  #  print "1,",   BonusDrink.total_count_for(1),"\n"
-  #  print "3,",   BonusDrink.total_count_for(3),"\n"
-  #  print "4,",   BonusDrink.total_count_for(4),"\n"
-  #  print "5,",   BonusDrink.total_count_for(5),"\n"
-  #  print "6,",   BonusDrink.total_count_for(6),"\n"
-  #  print "7,",   BonusDrink.total_count_for(7),"\n"
-  #  print "8,",   BonusDrink.total_count_for(8),"\n"
-  #  print "11,",  BonusDrink.total_count_for(11),"\n"
-  #  print "100,", BonusDrink.total_count_for(100),"\n"
-  #end
+  begin
+    print "0,",   BonusDrink.total_count_for(0),"\n"
+    print "1,",   BonusDrink.total_count_for(1),"\n"
+    print "3,",   BonusDrink.total_count_for(3),"\n"
+    print "4,",   BonusDrink.total_count_for(4),"\n"
+    print "5,",   BonusDrink.total_count_for(5),"\n"
+    print "6,",   BonusDrink.total_count_for(6),"\n"
+    print "7,",   BonusDrink.total_count_for(7),"\n"
+    print "8,",   BonusDrink.total_count_for(8),"\n"
+    print "11,",  BonusDrink.total_count_for(11),"\n"
+    print "73,",  BonusDrink.total_count_for(73),"\n"
+    print "100,", BonusDrink.total_count_for(100),"\n"
+  end
 
 end
